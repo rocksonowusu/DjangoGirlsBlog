1fa45a46763cdad0bbbe8e873e8e2e77a4bdac72
Rockson <orockson02@gmail.com>
2025-04-25 00:13:53 +0100

    Tweaked a few things about authentication in the views

--- blog/views.py
+++ blog/views.py
@@ -9,8 +9,12 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html',{'posts':posts})
+    if request.user.is_authenticated:
+        posts = Post.objects.filter(author=request.user, published_date__lte=timezone.now()).order_by('published_date')
+    else:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
@@ -30,6 +34,7 @@ def post_new(request):
     else:
         form = PostForm()
         return render(request,'blog/post_edit.html',{'form':form})
+    
 @login_required    
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
