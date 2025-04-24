02ff63a8f2311bcca26b279e72ffcc2f509f2c6b
Rockson <orockson02@gmail.com>
2025-04-25 00:06:27 +0100

    Added authentication system

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from .import views
 
 urlpatterns = [
@@ -6,5 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit,name='post_edit'),
+    path('accounts/', include('django.contrib.auth.urls')), 
+    path('signup/', views.signup, name='signup'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts':posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request,'blog/post_details.html',{'post':post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -24,7 +30,7 @@ def post_new(request):
     else:
         form = PostForm()
         return render(request,'blog/post_edit.html',{'form':form})
-    
+@login_required    
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,3 +44,14 @@ def post_edit(request, pk):
     else:
         form =  PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/signup.html',{'form':form})
